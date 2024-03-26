a280d4b07893c239587f9e3990742ca9828f94fc
dinesh8952 <129853237+dinesh8952@users.noreply.github.com>
2024-03-26 16:12:42 +0530

    Update game.py

--- my_python_game/game.py
+++ my_python_game/game.py
@@ -1,17 +1,16 @@
-# game.py
-
 import random
 
 def guess_number():
+    """
+    This function implements a number guessing game.
+    """
     number = random.randint(1, 10)
     while True:
         guess = int(input("Guess a number between 1 and 10: "))
         if guess == number:
             print("Congratulations! You guessed the correct number.")
             break
-        else:
-            print("Sorry, try again.")
+        print("Sorry, try again.")
 
 if __name__ == "__main__":
     guess_number()
-
